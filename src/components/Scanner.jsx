638a491d5df40275aa2e26d0b1ec9b91cf931781
Yhethi <eloyjosebonillag@gmail.com>
2024-09-01 20:17:48 -0400

    Updated

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,52 +1,52 @@
 import React, { useEffect } from "react";
 import Quagga from "quagga";
 
-const initScan = () => {
-  const [codeScanned, setCodeScanned] = useState(0);
-
-  Quagga.init(
-    {
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector("#reader"), // Or '#yourElement' (optional)
-      },
-      decoder: {
-        readers: [
-          "ean_reader",
-          "ean_reader",
-          "ean_8_reader",
-          "code_39_reader",
-          "code_39_vin_reader",
-          "codabar_reader",
-          "upc_reader",
-          "upc_e_reader",
-          "i2of5_reader",
-          "2of5_reader",
-          "code_93_reader",
-        ],
-      },
-    },
-    function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
-      Quagga.OnDetected((data) => {
-        showCode(data);
-      });
-    }
-  );
-};
-
-const showCode = (code) => {
-  console.log("Este es el codigo: ", code);
-  setCodeScanned(data);
-};
-
 export const Scanner = () => {
+    const [codeScanned, setCodeScanned] = useState(0);
+
+    const initScan = () => {
+        Quagga.init(
+          {
+            inputStream: {
+              name: "Live",
+              type: "LiveStream",
+              target: document.querySelector("#reader"), // Or '#yourElement' (optional)
+            },
+            decoder: {
+              readers: [
+                "ean_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "code_39_vin_reader",
+                "codabar_reader",
+                "upc_reader",
+                "upc_e_reader",
+                "i2of5_reader",
+                "2of5_reader",
+                "code_93_reader",
+              ],
+            },
+          },
+          function (err) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("Initialization finished. Ready to start");
+            Quagga.start();
+            Quagga.OnDetected((data) => {
+              showCode(data);
+            });
+          }
+        );
+      };
+      
+      const showCode = (code) => {
+        console.log("Este es el codigo: ", code);
+        setCodeScanned(data);
+      };
+
   return (
     <div>
       <h1>Scanner</h1>
